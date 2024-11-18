3b9415d3d751f0c67a84b372217c3f5ff3ab63b5
pinhalwalter <walterpinhal250@gmail.com>
2024-11-18 10:05:48 -0300

    Aprendendo sobre 'redux'

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Tela404 from "./componentes/Telas/Tela404";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TelaLogin from "./componentes/Telas/TelaLogin";
 import { useState, createContext } from "react";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 export const ContextoUsuario = createContext();
 
@@ -26,19 +28,21 @@ function App() {
   else {
     return (
       <div className="App">
-        <ContextoUsuario.Provider value={{ usuario, setUsuario }}>
-          <BrowserRouter>
-            { //A ordem das rotas é importante 
-            }
-            <Routes>
-              <Route path="/produto" element={<TelaCadastroProduto />} />
-              <Route path="/categoria" element={<TelaCadastroCategoria />} />
-              <Route path="/login" element={<TelaLogin />} />
-              <Route path="/" element={<TelaMenu />} />
-              <Route path="*" element={<Tela404 />} />
-            </Routes>
-          </BrowserRouter>
-        </ContextoUsuario.Provider>
+        <Provider store={store}>
+          <ContextoUsuario.Provider value={{ usuario, setUsuario }}>
+            <BrowserRouter>
+              { //A ordem das rotas é importante 
+              }
+              <Routes>
+                <Route path="/produto" element={<TelaCadastroProduto />} />
+                <Route path="/categoria" element={<TelaCadastroCategoria />} />
+                <Route path="/login" element={<TelaLogin />} />
+                <Route path="/" element={<TelaMenu />} />
+                <Route path="*" element={<Tela404 />} />
+              </Routes>
+            </BrowserRouter>
+          </ContextoUsuario.Provider>
+        </Provider>
       </div>
     );
   }
--- src/componentes/Telas/TelaCadastroProduto.jsx
+++ src/componentes/Telas/TelaCadastroProduto.jsx
@@ -4,11 +4,11 @@ import Pagina from "../layouts/Pagina";
 import { useEffect, useState } from "react";
 import TabelaProdutos from "./Tabelas/TabelaProdutos";
 //import { produtos } from "../../dados/mockProdutos";
-import { consultarProduto } from "../../servicos/servicoProduto";
+//import { consultarProduto } from "../../servicos/servicoProduto";
 
 export default function TelaCadastroProduto(props) {
     const [exibirTabela, setExibirTabela] = useState(true);
-    const [listaDeProdutos, setListaDeProdutos] = useState([]);
+    //const [listaDeProdutos, setListaDeProdutos] = useState([]);
     const [modoEdicao, setModoEdicao] = useState(false);
     //const [produtos, setProdutos] = useState([]);
     const [produtoSelecionado, setProdutoSelecionado] = useState({
@@ -39,19 +39,14 @@ export default function TelaCadastroProduto(props) {
                 </Alert>
                 {
                     exibirTabela ?
-                        <TabelaProdutos listaDeProdutos={listaDeProdutos}
-                                        setListaDeProdutos={setListaDeProdutos} 
-                                        setExibirTabela={setExibirTabela}
+                        <TabelaProdutos setExibirTabela={setExibirTabela}
                                         setModoEdicao={setModoEdicao}
                                         setProdutoSelecionado={setProdutoSelecionado} /> :
-                        <FormCadProdutos listaDeProdutos={listaDeProdutos}
-                                         setListaDeProdutos={setListaDeProdutos}
-                                         setExibirTabela={setExibirTabela}
+                        <FormCadProdutos setExibirTabela={setExibirTabela}
                                          produtoSelecionado={produtoSelecionado}
                                          setProdutoSelecionado={setProdutoSelecionado}
                                          modoEdicao={modoEdicao}
                                          setModoEdicao={setModoEdicao}
-
                                          />
                 }
             </Pagina>
